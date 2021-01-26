804b59c08bd461b51c02767b6f85d134a74599df
ShiningElektra <elektrathanos@googlemail.com>
2021-01-26 14:43:41 +0100

    Add input password

--- src/components/input/input.stories.js
+++ src/components/input/input.stories.js
@@ -1,10 +1,13 @@
 import "./input.css";
-import inputForm from "./input.html";
+import inputEmail from "./input-email.html";
+import inputPassword from "./input-password.html";
 
 export default {
   title: "Components/Input",
   parameters: { layout: "centered" },
 };
 
-export const input = () => inputForm;
+export const email = () => inputEmail;
+export const password = () => inputPassword;
+
 
