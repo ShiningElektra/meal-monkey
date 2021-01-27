9eda79edaf8fe50727a5853813b694485b719fb3
ShiningElektra <elektrathanos@googlemail.com>
2021-01-27 17:02:09 +0100

    Add Card-Container and grid styling

--- src/components/cards/cards.stories.js
+++ src/components/cards/cards.stories.js
@@ -2,6 +2,11 @@ import "./cards.css";
 import cardOffers from "./card-offers.html";
 import cardItalian from "./card-italian.html";
 import cardMexican from "./card-mexican.html";
+import cardArabic from "./card-arabic.html";
+import cardGreek from "./card-greek.html";
+import cardIndian from "./card-indian.html";
+import cardContainer from "./card-container.html";
+
 
 
 export default {
@@ -12,3 +17,8 @@ export default {
 export const offersCard = () => cardOffers;
 export const italianCard = () => cardItalian;
 export const mexicanCard = () => cardMexican;
+export const arabicCard = () => cardArabic;
+export const greekCard = () => cardGreek;
+export const indianCard = () => cardIndian;
+export const container = () => cardContainer;
+container.parameters = {layout:"fullscreen"};
