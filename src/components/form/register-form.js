9d6448089bcc0c024ace02f409b7f6d979bd7908
ShiningElektra <elektrathanos@googlemail.com>
2021-02-01 15:58:07 +0100

    set function in js

---
+++ src/components/form/register-form.js
@@ -0,0 +1,20 @@
+{/* <form class="form">
+    <h2>Reset Password</h2>
+    <p>
+    Please enter your email to receive a link to create a new password via email</p>
+    <input placeholder="email" />
+    <input type="submit" />
+    </form> */}
+
+export function createRegisterForm() {
+const button=document.createElement("button");
+button.innerText="Register";
+
+function register(){
+    alert("Registered");
+}
+
+button.addEventListener("click", register);
+
+return button;
+}
