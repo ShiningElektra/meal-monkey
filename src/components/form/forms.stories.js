78a56ac7954cd8090f6c5e861c978f02a6b6f700
ShiningElektra <elektrathanos@googlemail.com>
2021-02-02 17:12:27 +0100

    create verify side

--- src/components/form/forms.stories.js
+++ src/components/form/forms.stories.js
@@ -1,10 +1,11 @@
-import { format } from "prettier";
 import "./form.css";
-import {createRegisterForm } from "./register-form";
+import { createRegisterForm } from "./register-form";
+import { createVerifyForm } from "./verify-form";
 
-export default{
-    title:"Components/Form",
-parameters: { layout:"centered" },
+export default {
+  title: "Components/Form",
+  parameters: { layout: "centered" },
 };
 
 export const register = () => createRegisterForm();
+export const verify = () => createVerifyForm();
