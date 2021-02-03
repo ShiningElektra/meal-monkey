6b45a7af9680f5647f6762cb05dfa8bf3a7581d0
ShiningElektra <elektrathanos@googlemail.com>
2021-02-03 16:11:14 +0100

    ♻️ Add function createElement

--- src/components/form/verify-form.js
+++ src/components/form/verify-form.js
@@ -1,9 +1,12 @@
 import { doc } from "prettier";
+import { createElement } from "../utils/createElement";
+
 function createOTPInputElement() {
-  const input = document.createElement("input");
-  input.type = "password";
-  input.placeholder = "*";
-  input.className = "input";
+  const input = createElement("input", {
+    className: "input",
+    placeholder: "*",
+    type: "password",
+  });
 
   return input;
 }
@@ -12,12 +15,14 @@ export function createVerifyForm() {
   const verify = document.createElement("verify");
   verify.className = "verify";
 
-  const headLine = document.createElement("h2");
-  headLine.innerText = "We have send an OTP to your mobile";
+  const headLine = createElement("h2", {
+    innerText: "We have send an OTP to your mobile",
+  });
 
-  const subline = document.createElement("subline");
-  subline.innerText =
-    "Please check your mobile number 017******36 continue to reset your password";
+  const subline = createElement("subline", {
+    innerText:
+      "Please check your mobile number 017******36 continue to reset your password",
+  });
 
   const otpOne = createOTPInputElement();
   const otpTwo = createOTPInputElement();
